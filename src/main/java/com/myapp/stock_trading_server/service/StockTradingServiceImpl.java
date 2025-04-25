058829edd5fa0f0e9a776faeaaac2dee2dce47a6
Nuwan <nuwan199044@gmail.com>
2025-04-26 01:12:18 +0530

    server streaming.

--- src/main/java/com/myapp/stock_trading_server/service/StockTradingServiceImpl.java
+++ src/main/java/com/myapp/stock_trading_server/service/StockTradingServiceImpl.java
@@ -9,6 +9,10 @@ import io.grpc.stub.StreamObserver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.grpc.server.service.GrpcService;
 
+import java.time.Instant;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
 @GrpcService
 @RequiredArgsConstructor
 public class StockTradingServiceImpl extends StockTradingServiceGrpc.StockTradingServiceImplBase {
@@ -29,5 +33,25 @@ public class StockTradingServiceImpl extends StockTradingServiceGrpc.StockTradin
 
         responseObserver.onNext(stockResponse);
         responseObserver.onCompleted();
+    }
+
+    @Override
+    public void subscribeStockPrice(StockRequest request, StreamObserver<StockResponse> responseObserver) {
+        String symbol = request.getStockSymbol();
+
+        try {
+            for (int i =0; i < 10; i++) {
+                StockResponse stockResponse = StockResponse.newBuilder()
+                        .setStockSymbol(symbol)
+                        .setPrice(new Random().nextDouble(200))
+                        .setTimestamp(Instant.now().toString())
+                        .build();
+                responseObserver.onNext(stockResponse);
+                TimeUnit.SECONDS.sleep(1);
+            }
+            responseObserver.onCompleted();
+        } catch (Exception ex) {
+            responseObserver.onError(ex);
+        }
     }
 }
